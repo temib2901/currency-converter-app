cb7c26542581e45a1ab6484791bf6153c9f03daa
Temitope Babayomi <topebabayomi@gmail.com>
2024-07-06 22:12:06 -0400

    first commit

---
+++ currency-converter-app/src/main/java/com/bptn/individual/project/currency_converter_app/App.java
@@ -0,0 +1,13 @@
+package com.bptn.individual.project.currency_converter_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
